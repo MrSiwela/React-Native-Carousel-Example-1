e9d4e31b2972e48c967af0285269d17e4a9f6957
Sanele <mrsiwela94@gmail.com>
2022-01-04 10:59:35 +0200

    added rotating square in background

--- App.js
+++ App.js
@@ -78,9 +78,23 @@ const Backdrop = ({scrollX})=>{
 }
 
 const Square = ({scrollX})=>{
+  const YOLO = Animated.modulo(
+    Animated.divide(Animated.modulo(scrollX,width),new Animated.Value(width)),
+    1);
+  
+  const rotate = YOLO.interpolate({
+    inputRange: [0,0.5,1],
+    outputRange: ['35deg','0deg','35deg']
+  });
+
+  const translateX = YOLO.interpolate({
+    inputRange: [0,0.5,1],
+    outputRange: [0,-height,0]
+  });
+
   return(
     <Animated.View
-      style={{width: height,height: height,top: -height*0.65,left: -height*0.3,backgroundColor: '#fff',borderRadius: 86,position: 'absolute',transform: [{rotate: '35deg'}]}}
+      style={{width: height,height: height,top: -height*0.65,left: -height*0.3,backgroundColor: '#fff',borderRadius: 86,position: 'absolute',transform: [{rotate},{translateX}]}}
     />
   )
 }
